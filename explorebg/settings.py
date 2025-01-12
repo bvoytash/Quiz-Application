4ad8a326720e24aa0cdd7bd63e3fa6d468bce7c6
Borislav  Voytash <borislavvoytash@Borislavs-MacBook-Air.local>
2025-01-12 20:57:42 +0200

    host

--- explorebg/settings.py
+++ explorebg/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+e47zn(btsf^fcs_@605h6ish*
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quiz-application-3vdp.onrender.com']
 
 
 INSTALLED_APPS = [
