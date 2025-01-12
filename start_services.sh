6edbe58a9bdff9dab002c3ff67b7abfd8a1f11a7
Borislav  Voytash <borislavvoytash@Borislavs-MacBook-Air.local>
2025-01-12 23:23:51 +0200

    create superuser when start

--- start_services.sh
+++ start_services.sh
@@ -12,9 +12,16 @@
 # Start Celery worker in the background
 nohup celery -A explorebg worker --loglevel=info &
 
-
 #make migrations
 python manage.py migrate
+
+
+#createsuperuser
+export $(cat .env | grep -v ^# | xargs)
+
+# Create superuser with credentials from .env
+echo -e "admin\n$SUPER_USER\n$SUPER_USER_PASS\n$SUPER_USER_PASS" | python manage.py createsuperuser --noinput
+
 
 
 # Start Django server in the current terminal window
